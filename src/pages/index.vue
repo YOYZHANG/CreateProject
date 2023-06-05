054e96c949ef90e32f34f45f916063e7fa66295d
= <=>
2023-06-05 18:20:55 +0800

    fix: Input

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,12 +2,6 @@
 import { ref } from 'vue'
 import { useRouter } from 'vue-router/auto'
 
-import TheInput from '../components/TheInput.vue'
-
-defineOptions({
-  name: 'IndexPage',
-})
-
 const name = ref('')
 
 const router = useRouter()
@@ -20,20 +14,28 @@ function go() {
 <template>
   <div>
     <div inline-block text-4xl i-carbon-3d-cursor />
-    <p px-4 py-4 text-xl>
+    <p px-4>
       <a href="https://github.com/YOYZHANG/originApp">Origin App</a>
     </p>
 
-    <TheInput
-      v-model="name"
-      placeholder="What's your name?"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
     <div>
-      <button m-6 text-sm :disabled="!name" @click="go">
-        GO!
-      </button>
+      <input
+        id="input"
+        v-model="name"
+        type="text"
+        p="x-4 y-2"
+        text="center"
+        bg="transparent"
+        border="~ rounded gray-200 dark:gray-700"
+        outline="none active:none"
+        placeholder="What's your name?"
+        autocomplete="false"
+        @keydown.enter="go"
+      >
     </div>
+
+    <button m-3 text-sm :disabled="!name" @click="go">
+      GO!
+    </button>
   </div>
 </template>
